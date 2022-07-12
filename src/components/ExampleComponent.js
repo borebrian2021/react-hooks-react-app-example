45a1071ff13ca9fd094a4361345ffb973025c66c
Brian <bkimutai2021@gmail.com>
2022-07-12 16:52:08 +0300

    Done.

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -4,10 +4,7 @@ import React from "react";
 function ExampleComponent() {
   return (
     <div className="example">
-      <img
-        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
-        alt="a deeply impressed man, perhaps thinking 'whoa'"
-      />
+      <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
       <p>Whoa!</p>
     </div>
   );
